4d9a0a2bb6ec6e26b9e63acc7f0a9f52b46e7ab0
Juan P Murdolo <40174725+JuanPMurdolo@users.noreply.github.com>
2022-12-22 09:57:35 -0300

    Persistencia en clase Bank
    Persistencia de la clase bank para tener como modelo

--- tarjetas/src/main/java/unlp/basededatos/tarjetas/model/Bank.java
+++ tarjetas/src/main/java/unlp/basededatos/tarjetas/model/Bank.java
@@ -1,13 +1,25 @@
 package unlp.basededatos.tarjetas.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Bank")
 public class Bank {
 
+    @Column(name = "name", length = 100)
     private String name;
 
+    @Id
+    @Column(name = "cuit", unique = true)
     private String cuit;
 
+    @Column(name = "address")
     private String address;
 
+    @Column(name = "telephone")
     private String telephone;
 
     public Bank(String name, String cuit, String address, String telephone) {
@@ -15,6 +27,10 @@ public class Bank {
         this.cuit = cuit;
         this.address = address;
         this.telephone = telephone;
+    }
+
+    public Bank(){
+
     }
 
     public String getName() {
