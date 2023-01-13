be70281e01907ac4a8b8a5a8012cb58875bed15f
Juan P Murdolo <jpmurdolo@gmail.com>
2023-01-13 16:48:38 -0300

    Create Card.java

---
+++ tarjetas/tarjetas/src/main/java/unlp/basededatos/tarjetas/model/Card.java
@@ -0,0 +1,80 @@
+package unlp.basededatos.tarjetas.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Card")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name ="id_card")
+    private Long id;
+
+    @Column(name ="number")
+    private String number;
+
+    @Column(name ="ccv")
+    private String ccv;
+
+    @Column(name ="cardholder")
+    private String cardholderNameInCard;
+
+    @Column(name ="since")
+    private Date since;
+
+    @Column(name ="expiration")
+    private Date expirationDate;
+
+    public Card(String number, String ccv, String cardholderNameInCard, Date since, Date expirationDate) {
+        this.number = number;
+        this.ccv = ccv;
+        this.cardholderNameInCard = cardholderNameInCard;
+        this.since = since;
+        this.expirationDate = expirationDate;
+    }
+
+    public Card() {
+
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCcv() {
+        return ccv;
+    }
+
+    public void setCcv(String ccv) {
+        this.ccv = ccv;
+    }
+
+    public String getCardholderNameInCard() {
+        return cardholderNameInCard;
+    }
+
+    public void setCardholderNameInCard(String cardholderNameInCard) {
+        this.cardholderNameInCard = cardholderNameInCard;
+    }
+
+    public Date getSince() {
+        return since;
+    }
+
+    public void setSince(Date since) {
+        this.since = since;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
