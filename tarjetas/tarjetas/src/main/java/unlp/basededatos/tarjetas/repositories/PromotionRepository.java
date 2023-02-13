e6cd26037cc0b073423ff8bae704b695c82f3ad7
Juan P Murdolo <jpmurdolo@gmail.com>
2023-02-13 10:09:32 -0300

    WIP JpaRepo Purchase

--- tarjetas/tarjetas/src/main/java/unlp/basededatos/tarjetas/repositories/PromotionRepository.java
+++ tarjetas/tarjetas/src/main/java/unlp/basededatos/tarjetas/repositories/PromotionRepository.java
@@ -1,77 +1,15 @@
 package unlp.basededatos.tarjetas.repositories;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import unlp.basededatos.tarjetas.model.Promotion;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
-import unlp.basededatos.tarjetas.model.Promotion;
-import unlp.basededatos.tarjetas.utils.TarjetasException;
+public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+    @Query(nativeQuery = true, value ="SELECT p FROM Promotion p WHERE promotion.cuit = :cuit AND p.validityStartDate >= :date AND p.validityEndDate <= :date1")
+    public List<Promotion> findPromotionByCuitByDate(@Param("cuit") String cuit, @Param("date") Date date, @Param("date1") Date date1);
 
-@Repository
-public class PromotionRepository {
-
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    public Long savePromotion(Promotion promotion) throws TarjetasException {
-        Session session = null;
-        try {
-            session = this.sessionFactory.getCurrentSession();
-            return (Long) session.save(promotion);
-        } catch (Exception e) {
-            throw new TarjetasException(e.getMessage());
-        }
-    }
-
-    public Promotion findPromotionById(Long id) throws TarjetasException {
-        try {
-            return (Promotion) this.sessionFactory.getCurrentSession().createQuery("from Promotion where id = :id").setParameter("id", id).uniqueResult();
-        } catch (Exception e) {
-            throw new TarjetasException(e.getMessage());
-        }
-    }
-    
-    public Promotion findPromotionByCode(String code) throws TarjetasException {
-        try {
-            return (Promotion) this.sessionFactory.getCurrentSession().createQuery("from Promotion where code = :code").setParameter("code", code).uniqueResult();
-        } catch (Exception e) {
-            throw new TarjetasException(e.getMessage());
-        }
-    }
-
-    public void updatePromotion(Promotion promotion) throws TarjetasException {
-        Session session = null;
-        try {
-            session = this.sessionFactory.getCurrentSession();
-            session.save(promotion);
-        } catch (Exception e) {
-            throw new TarjetasException(e.getMessage());
-        }
-    }
-
-	public void deletePromotion(Promotion promotion) throws TarjetasException {
-        Session session = null;
-        try {
-            session = this.sessionFactory.getCurrentSession();
-            promotion.setBorrado(true);
-            session.save(promotion);
-        } catch (Exception e) {
-            throw new TarjetasException(e.getMessage());
-        }		
-	}
-
-    public List<Promotion> findPromotionByCuitByDate(String cuit, Date date, Date date1) throws TarjetasException{
-        try {
-            return (List<Promotion>) this.sessionFactory.getCurrentSession().createQuery("from Promotion where cuitStore = :cuit and validityStartDate >= :date or validityEndDate <= :date1").setParameter("cuit", cuit).setParameter("date", date).setParameter("date1", date1).getResultList();
-        } catch (Exception e) {
-            throw new TarjetasException(e.getMessage());
-        }
-    }
-    
 }
